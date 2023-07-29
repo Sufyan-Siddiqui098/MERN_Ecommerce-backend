e07f966ade91eaf7174755b63b7310c8aebdb9bb
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-07-29 22:46:04 +0500

    SignUp and Login added

---
+++ controllers/authControllers.js
@@ -0,0 +1,126 @@
+const userModel = require("../models/userModel")
+const {createHashPassword, comparePassword} = require("../helpers/authHelper")
+const JWT = require("jsonwebtoken")
+
+
+const registerController = async (req, res)=> {
+    try{
+        const {name, email, password, phone, address} = req.body;
+
+        //validation
+        if(!name){
+            return res.send({error: "Name is required"})
+        }
+        if(!email){
+            return res.send({error: "Email is required"})
+        }
+        if(!password){
+            return res.send({error: "Password is required"})
+        }
+        if(!phone){
+            return res.send({error: "Phone is required"})
+        }
+        if(!address){
+            return res.send({error: "Address is required"})
+        }
+
+        //Check user
+        const existingUser = await  userModel.findOne({email})
+        //existing user
+        if(existingUser){
+            return res.status(200).send({
+                success: true,
+                message: "Already Registered please login"
+            })
+        }
+
+        //register new User.
+        const hashedPassword = await createHashPassword(password);
+        const user = await userModel.create({
+            name,
+            phone,
+            email,
+            password: hashedPassword,
+            address,
+        })
+
+        res.status(200).send({
+            success: true,
+            message: "User registered successfully !",
+            user
+        })
+
+
+    } catch(error) {
+        res.status(500).send({
+            success: false,
+            message: "Error in registration",
+            error: error
+        })
+    }       
+}
+
+
+//POST - Login
+const loginController = async(req, res) => {
+    try{
+        const {email, password} = req.body;
+        
+        //validation
+        if(!email || !password){
+            return res.status(404).send({
+                success: false,
+                message: "Invalid email or password",
+            })
+        }
+
+        const user = await userModel.findOne({email})
+        //If user not found with the email provided
+        if(!user){
+            return res.status(404).send({
+                success: false,
+                message: "Email is not registered"
+            })
+        }
+
+        const matchPass = await comparePassword(password, user.password);
+
+        //if password doesn't matcht the existing password
+        if(!matchPass){
+            return res.status(400).send({
+                success: false,
+                message: "Invalid Password"
+            })
+        }
+
+        //Token
+        const token = await JWT.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: "7d"})
+
+        res.status(200).send({
+            success: true,
+            message: "Login Successfully",
+            user: {
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address
+            },
+            token
+        })
+
+
+    } catch(err){
+        res.status(500).send({
+            success: false,
+            message: "Error in Login",
+            error: err
+        })
+    }
+}
+
+//Test Controller
+const testController = (req, res) => {
+    res.send("Protected Routes")
+}
+
+module.exports = {registerController, loginController, testController}
