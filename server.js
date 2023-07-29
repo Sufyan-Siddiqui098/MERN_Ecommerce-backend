c3cff0935768f4d550f1e373d61bbc6927837f28
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-07-29 22:45:07 +0500

    env and route for auth added

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const dotenv = require("dotenv")
 const connectDB = require("./db")
-
+const authRoutes = require("./routes/authRoutes")
 //Environment Variable Configured.
 dotenv.config();
+const PORT = process.env.PORT
 
 app.use(express.json())
 
-const PORT = process.env.PORT
+//routes
+app.use('/api/v1/auth', authRoutes)
+
 
 app.get("/", (req, res)=>{
     res.send("Welcome to get request..")
