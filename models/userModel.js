d758f1938dd147dc1b8fbedd4ef7b0d76c0265e5
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-07-29 22:46:56 +0500

    User Schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,34 @@
 const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+
+const userSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique : true
+    },
+    password: {
+        type: String,
+        required: true,    
+    },
+    phone : {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: Number,
+        default: 0 //0 means false 1 means true.
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('user', userSchema)
