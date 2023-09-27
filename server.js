64d6d0a107499347160af8b6a83907aa623a8a75
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-09-28 02:18:42 +0500

    category route added inside server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const dotenv = require("dotenv")
 const connectDB = require("./db")
 const authRoutes = require("./routes/authRoutes")
+const categoryRoutes = require('./routes/categoryRoutes')
 //Environment Variable Configured.
 dotenv.config();
 const PORT = process.env.PORT
@@ -13,7 +14,7 @@ app.use(express.json())
 
 //routes
 app.use('/api/v1/auth', authRoutes)
-
+app.use('/api/v1/category', categoryRoutes)
 
 app.listen(PORT, ()=>{
     connectDB() 
