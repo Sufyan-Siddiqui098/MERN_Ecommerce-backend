67535ae9929fed763457bf726337ea226fee841f
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-10-28 15:46:23 +0500

    indentation added and filter product route created

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,23 +1,50 @@
-const express = require('express');
-const { requireSignIn, isAdmin } = require('../middlewares/authMiddleware');
-const {createProductController, getProductsController, getSingleProductController, getProductPhotoController, deleteProductController, updateProductController} = require('../controllers/productControllers')
-const formidableMiddleware = require('express-formidable');
+const express = require("express");
+const { requireSignIn, isAdmin } = require("../middlewares/authMiddleware");
+const {
+  createProductController,
+  getProductsController,
+  getSingleProductController,
+  getProductPhotoController,
+  deleteProductController,
+  updateProductController,
+  filterProductController,
+} = require("../controllers/productControllers");
+const formidableMiddleware = require("express-formidable");
 
 const router = express.Router();
 
-
 // Create Product || POST
-router.post('/create-product', requireSignIn, isAdmin, formidableMiddleware(), createProductController )
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidableMiddleware(),
+  createProductController
+);
 //Get all products || GET
-router.get('/get-products', getProductsController)
+router.get("/get-products", getProductsController);
 //Get single Product || GET
-router.get('/get-product/:slug', getSingleProductController)
+router.get("/get-product/:slug", getSingleProductController);
 //Update Product || PUT
-router.put('/update-product/:pid', requireSignIn, isAdmin, formidableMiddleware(), updateProductController)
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidableMiddleware(),
+  updateProductController
+);
 //DELETE product
-router.delete('/delete-product/:pid',requireSignIn, isAdmin, deleteProductController) 
+router.delete(
+  "/delete-product/:pid",
+  requireSignIn,
+  isAdmin,
+  deleteProductController
+);
 
-//Get product PHOTO 
-router.get('/product-photo/:pid', getProductPhotoController);
+//Get product PHOTO
+router.get("/product-photo/:pid", getProductPhotoController);
 
-module.exports = router
+//Get filter product || POST
+router.post("/filter-product", filterProductController);
+
+module.exports = router;
